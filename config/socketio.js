503dfa0b68ffc70e7e2a6694407eda6724826f42
wcweb <wcrh35@gmail.com>
2014-03-26 09:50:35 +0800

    update socket.io config clust

--- config/socketio.js
+++ config/socketio.js
@@ -1,5 +1,17 @@
-module.exports = function (io){
+var RedisStore = require('socket.io/lib/stores/redis')
+    , redis  = require('socket.io/node_modules/redis')
+    , pub    = redis.createClient()
+    , sub    = redis.createClient()
+    , client = redis.createClient();
 
+
+
+module.exports = function (io){
+    io.set('store', new RedisStore({
+        redisPub : pub
+        , redisSub : sub
+        , redisClient : client
+    }));
     io.sockets.on('connection', function (socket) {
         socket.emit('news', { hello: 'world' });
         socket.on('my other event', function (data) {
@@ -12,5 +24,21 @@ module.exports = function (io){
             socket.broadcast.emit('someOneTalk',{msg:data.msg});
         })
 
+    });
+
+    io.configure('production', function(){
+        io.enable('browser client etag');
+        io.set('log level', 1);
+
+        io.set('transports', [
+            'websocket'
+            , 'flashsocket'
+            , 'htmlfile'
+            , 'xhr-polling'
+            , 'jsonp-polling'
+        ]);
+    });
+    io.configure('development', function(){
+        io.set('transports', ['websocket']);
     });
 }
